924127610ce79a68f173dcd32e0cba008d5c05ce
Mehmet Ali Özel <fizyo.maliozel@gmail.com>
2024-03-13 14:05:06 +0100

    HalloMislav

--- src/main/java/com/example/learningJournal/GreetingController.java
+++ src/main/java/com/example/learningJournal/GreetingController.java
@@ -12,7 +12,9 @@ public class GreetingController {
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+        System.out.println("Hallo Mislav!");
         return "greeting";
+
     }
 
 }
