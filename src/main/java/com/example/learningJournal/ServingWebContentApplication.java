8f6bcf772b9d64d4a4fd7d69db341cec8664ce92
Mehmet Ali Özel <fizyo.maliozel@gmail.com>
2024-03-13 14:01:50 +0100

    Commit von mehmet

--- src/main/java/com/example/learningJournal/ServingWebContentApplication.java
+++ src/main/java/com/example/learningJournal/ServingWebContentApplication.java
@@ -1,4 +1,14 @@
 package com.example.learningJournal;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ServingWebContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+
 }
