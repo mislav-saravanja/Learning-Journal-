706cfed3d6c34f163f21092679f830444a152e88
Mehmet Ali Özel <fizyo.maliozel@gmail.com>
2024-03-14 13:03:44 +0100

    von mehmet

--- src/main/java/com/example/learningJournal/GreetingController.java
+++ src/main/java/com/example/learningJournal/GreetingController.java
@@ -10,11 +10,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "Mislav") String name, Model model) {
         model.addAttribute("name", name);
-        System.out.println("Hallo Mislav!");
+        System.out.println(name);
         return "greeting";
 
     }
-
 }
+
