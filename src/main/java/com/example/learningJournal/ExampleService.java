c8aa49796adcc8f8600af0ae01caf890c2462fe0
groups <yusufsamil.oeztuerk@tn.mischok.academy>
2024-03-15 09:44:09 +0100

    kommentare von sebi

--- src/main/java/com/example/learningJournal/ExampleService.java
+++ src/main/java/com/example/learningJournal/ExampleService.java
@@ -1,4 +1,21 @@
 package com.example.learningJournal;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ExampleService {
+
+
+    public void handleAddUser(Greeting dto){
+        //Create User Enttity from String / DTO
+        //Call Repository to create user in DB
+    }
+
+    public List<Greeting> handleGetAllGreetings(){
+        //load all users from repository
+        //transfer UserEntity to Greeting DTO
+        return null; //change this
+    }
 }
--- src/main/java/com/example/learningJournal/GreetingController.java
+++ src/main/java/com/example/learningJournal/GreetingController.java
@@ -1,20 +1,35 @@
 package com.example.learningJournal;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "Mislav") String name, Model model) {
-        model.addAttribute("name", name);
-        System.out.println(name);
-        return "greeting";
+    @Autowired
+    private ExampleService exampleService;
 
+
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        //call service to get list of all users
+        //add them to model
+        return "greeting";
     }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("greeting", greeting);
+        //Call service to create greeting or use in database
+        return "result";
+    }
+
 }
 
