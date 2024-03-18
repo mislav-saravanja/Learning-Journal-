c61438899b0fde29c50fb61e333f5370ce37aa04
Mehmet Ali Özel <fizyo.maliozel@gmail.com>
2024-03-18 10:45:18 +0100

    routingVertig

--- src/main/java/com/example/learningJournal/HomeController.java
+++ src/main/java/com/example/learningJournal/HomeController.java
@@ -1,14 +1,27 @@
 package com.example.learningJournal;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-        @GetMapping("/home")
-        public String home() {
-            return "home";
-        }
+    @GetMapping("/home")
+    public String home() {
+        return "home";
     }
+
+    @GetMapping("/classes")
+    public String classes() {
+        return "classes";
+    }
+
+    @GetMapping("/journal")
+    public String journal() {
+        return "journal";
+    }
+
+
+}
+
 
 
